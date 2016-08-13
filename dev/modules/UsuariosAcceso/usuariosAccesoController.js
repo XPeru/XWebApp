fbff5433bb8fa09f8dfaac30cf8826a078863e2e
oscar garay <ojcgv21@gmail.com>
2016-08-13 13:58:26 +0200

    bug corrections usuarios acceso

--- dev/modules/UsuariosAcceso/usuariosAccesoController.js
+++ dev/modules/UsuariosAcceso/usuariosAccesoController.js
@@ -1,5 +1,5 @@
 "use strict";
-angular.module('UsuariosAcceso', ['ui.bootstrap', 'ui.grid'])
+angular.module('UsuariosAcceso', ['ui.bootstrap', 'ui.grid','ui.grid.exporter', 'ui.grid.selection'])
 	.controller('usuariosAccesoController', ['$scope',
 													'$location',
 													'$http',
@@ -10,48 +10,68 @@ angular.module('UsuariosAcceso', ['ui.bootstrap', 'ui.grid'])
 													'i18nService',
 		function($scope, $location, $http, $uibModal, $timeout, UsuariosAccesoServiceFactory, NgTableParams, i18nService) {
 			var ctrl = this;
+			ctrl.tableMode = true;
+			ctrl.switchTableMode = function() {
+				ctrl.tableMode = !ctrl.tableMode;
+				console.info("tableMode");
+				console.info(ctrl.tableMode);
+			};
+
 			i18nService.setCurrentLang('es');
+			$scope.columns = [{ field: 'DESCRIPCION', headerCellClass: 'blue'}];
+			$scope.columns[0].displayName = 'Descripcion';
+			$scope.gridOptions = {
+				exporterMenuCsv: false,
+				enableGridMenu: true,
+				enableSorting: true,
+				columnDefs: $scope.columns,
+				onRegisterApi: function(gridApi) {
+					$scope.gridApi = gridApi;
+					gridApi.core.on.columnVisibilityChanged( $scope, function( changedColumn ){
+						$scope.columnChanged = { name: changedColumn.colDef.name, visible: changedColumn.colDef.visible };
+					});
+				}
+			};
+
 			ctrl.usuariosAccesoData = [{}];
 			ctrl.modal_acceso_usuario_not_finished = true;
 
-			ctrl.sortType     = 'ID_ACCESO_USUARIO'; // set the default sort type
-			ctrl.sortReverse  = false;  // set the default sort order
+			ctrl.sortType     = 'ID_ACCESO_USUARIO';
+			ctrl.sortReverse  = false;
 
 			ctrl.setType = function(type) {
 				ctrl.sortType = type;
 				ctrl.sortReverse = !ctrl.sortReverse;
 			};
 
-			ctrl.usuariosAccesoTable = new NgTableParams({
-				page: 1,
-				count: 10
-			}, {
-				total: 0,
-				counts: [],
-				getData: function(params) {
-					if (ctrl.modal_acceso_usuario_not_finished) {
-							ctrl.callGetAllAccesoUsuario();
-						} else {
-							params.total(ctrl.usuariosAccesoData.length);
-							return ctrl.usuariosAccesoData;
-						}
-						ctrl.modal_acceso_usuario_not_finished = false;
-				}
-				
-			});
-			
-			ctrl.callGetAllAccesoUsuario = function() {
-				UsuariosAccesoServiceFactory.getAllAccesoUsuario().then(function(response) {
-					ctrl.usuariosAccesoData = response.data;
-					//ctrl.usuariosAccesoTable.reload();
-					console.info(ctrl.usuariosAccesoData.AccesosUsuario);
-				});
-			};
-			ctrl.callGetAllAccesoUsuario();
 			ctrl.idSelectedAccesoUsuario = null;
 			ctrl.setSelected = function(idSelectedAccesoUsuario) {
 				ctrl.idSelectedAccesoUsuario = idSelectedAccesoUsuario;
 			};
+
+			ctrl.usuariosAccesoTable = new NgTableParams({
+			}, {
+				getData: function(params) {
+					if (ctrl.modal_acceso_usuario_not_finished) {
+							ctrl.callGetAllAccesoUsuario();
+					} else {
+							params.total(ctrl.usuariosAccesoData.AccesosUsuario.length);
+							return ctrl.usuariosAccesoData;
+					}
+					ctrl.modal_acceso_usuario_not_finished = false;
+				}
+			});
+
+			ctrl.callGetAllAccesoUsuario = function() {
+				UsuariosAccesoServiceFactory.getAllAccesoUsuario().then(function(response) {
+					$scope.gridOptions.data = response.data.AccesosUsuario;
+					ctrl.usuariosAccesoData = response.data;
+					ctrl.usuariosAccesoTable.reload();
+				});
+			};
+			ctrl.callGetAllAccesoUsuario();
+
+			
 
 			ctrl.openModalAccesoUsuario = function(selected_modal, selected_acceso_usuario) {
 				var modalInstance = $uibModal.open({
@@ -80,12 +100,10 @@ angular.module('UsuariosAcceso', ['ui.bootstrap', 'ui.grid'])
 				});
 
 				modalInstance.result.then(function() {
-					ctrl.modal_acceso_usuario_not_finished = true;
-					ctrl.usuariosAccesoTable.reload();
+					ctrl.callGetAllAccesoUsuario();
 					
 				}, function() {
-					ctrl.modal_acceso_usuario_not_finished = true;
-					ctrl.usuariosAccesoTable.reload();
+					ctrl.callGetAllAccesoUsuario();
 				});
 			};
 
