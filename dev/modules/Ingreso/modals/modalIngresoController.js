58d089f348d61a0cdb4aff29f77f334c2a071549
oscar garay <ojcgv21@gmail.com>
2017-01-29 02:20:53 +0100

    modal delete ingreso

--- dev/modules/Ingreso/modals/modalIngresoController.js
+++ dev/modules/Ingreso/modals/modalIngresoController.js
@@ -28,33 +28,4 @@ angular.module('Ingreso').controller('modalIngresoController',  function ($scope
 		$uibModalInstance.dismiss('cancel');
 	};
 	
-	// function getDayClass(data) {
-	// 	var date = data.date,
-	// 		mode = data.mode;
-	// 		data = data*0;
-	// 		console.info("dato en dayclass");
-	// 		console.info(data);
-	// 		console.info("eventos en scope");
-	// 		console.info($scope.events);
-	// 	if (mode === 'day') {
-	// 		var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
-
-	// 		for (var i = 0; i < $scope.events.length; i++) {
-	// 			var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
-
-	// 			if (dayToCheck === currentDay) {
-	// 				return $scope.events[i].status;
-	// 			}
-	// 		}
-	// 	}
-	// }
-
-	// ctrl.options = {
-	// 	customClass: getDayClass,
-	// 	minDate: new Date(),
-	// 	showWeeks: true
-	// };
-
-	
-
 });
