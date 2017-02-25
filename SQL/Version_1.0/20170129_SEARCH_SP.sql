58cfec311cbce6777b5551956f411e82d036b978
eduapb <Alex@Alexs-MacBook-Pro.local>
2017-02-25 22:13:08 +0900

    search_string

--- SQL/Version_1.0/20170129_SEARCH_SP.sql
+++ SQL/Version_1.0/20170129_SEARCH_SP.sql
@@ -25,3 +25,14 @@ BEGIN
     DEALLOCATE PREPARE stmt;  
 END$$
 DELIMITER ;
+
+DELIMITER $$
+USE testdb$$
+CREATE PROCEDURE `SP_SEARCH_STRING` (IN tab_name varchar(100), IN col_name varchar(50), IN col_value varchar(50))
+BEGIN
+	SET @query = CONCAT ('SELECT * FROM ',tab_name,' WHERE ',col_name,' = `',col_value,'`');
+    PREPARE stmt FROM @query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;  
+END$$
+DELIMITER ;
