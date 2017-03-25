9e22e8f52d91889f0af70def04ccea20696dcc05
oscar garay <oscar.garay-velasquez@talanlabs.com>
2017-03-25 16:13:34 +0100

    login update

--- dev/modules/Login/loginController.js
+++ dev/modules/Login/loginController.js
@@ -1,37 +1,29 @@
-(function () {
-    //'use strict';
-     function loginController ($scope, $location, LoginServiceFactory) {
-        //var viewLogin = this;
-        console.info("loginController");
-        //viewLogin.login = login;
+'use strict';
  
-        /*(function initController() {
-            // reset login status
-            console.info("init controller");
-            LoginServiceFactory.ClearCredentials();
-        })();*/
+angular
+    .module('Login',[])
+    .controller('loginController', ['$scope', '$location', 'LoginServiceFactory', function loginController ($scope, $location, LoginServiceFactory) {
+    //var viewLogin = this;
+    console.info("loginController");
+    //viewLogin.login = login;
+
+    /*(function initController() {
+        // reset login status
+        console.info("init controller");
         LoginServiceFactory.ClearCredentials();
-        $scope.login = function (loginUser) {
-            console.info("login controller, LoginServiceFactory.Login");
-            loginUser.dataLoading = true;
-            LoginServiceFactory.Login(loginUser.username, loginUser.password, function (response) {
-                if (response.Users.length === 1) {
-                    LoginServiceFactory.SetCredentials(loginUser.username, loginUser.password);
-                    $location.path('/home');
-                } else {/*
-                    FlashService.Error(response.message);*/
-                    loginUser.dataLoading = false;
-                }
-            });
-        };
-    }
-    angular
-        .module('Login',[])
-        .controller('loginController', loginController);
- 
-    
-
-
-    loginController.$inject = ['$scope', '$location', 'LoginServiceFactory'];
- 
-})();
+    })();*/
+    LoginServiceFactory.ClearCredentials();
+    $scope.login = function (loginUser) {
+        console.info("login controller, LoginServiceFactory.Login");
+        loginUser.dataLoading = true;
+        LoginServiceFactory.Login(loginUser.username, loginUser.password, function (response) {
+            if (response.Users.length === 1) {
+                LoginServiceFactory.SetCredentials(loginUser.username, loginUser.password);
+                $location.path('/home');
+            } else {/*
+                FlashService.Error(response.message);*/
+                loginUser.dataLoading = false;
+            }
+        });
+    };
+}]);
