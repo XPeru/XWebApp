daaa7c8fe9f43906af2fd96a0aec52d87f302c86
Oscar Garay <ojcgv21@gmail.com>
2017-07-16 01:43:01 +0200

    first directive + refactoring

--- dev/modules/NavBar/nav.js
+++ dev/modules/NavBar/nav.js
@@ -1,10 +1,10 @@
 "use strict";
 angular.module('NavBar', [])
-	
+
 	.controller('navBarController', ['$scope', '$rootScope', function ($scope, $rootScope) {
-		$scope.comment ="this is usless";
-		$rootScope.comment ="this is usless";
-	}
+			$scope.comment ="this is usless";
+			$rootScope.comment ="this is usless";
+		}
 	])
 
 	.directive('xwebappNavBar',
