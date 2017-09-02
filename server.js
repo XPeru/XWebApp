0b5df4f5ea9315192ae51a5643e8b8cbd63655b1
Oscar Garay <ojcgv21@gmail.com>
2017-09-02 17:40:39 +0200

    first refactoring backend

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 //Express is a minimalist web framework for node.js
-var express = require("express");
+const express = require("express");
 // This module allows our server to connect and dialog with a mysql database
-var mysql = require("mysql");
+const mysql = require("mysql");
 // This allows our server to parse JSONs objects
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 // npm install serve-favicon
-var favicon = require("serve-favicon");
-// I don't know :)
-var md5 = require('MD5');
+const favicon = require("serve-favicon");
+// For hash encryption, used for passwords
+const md5 = require('MD5');
+const colors = require('colors');
+const path = require("path");
 
-var colors = require('colors');
 // The rest.js contains all the routes for the server, it should be composed by many others files
 // One file per application service
 var restUsuarios = require("./DAO/usuariosDAO.js");
 var restArticulos = require("./DAO/articulosDAO.js");
 var restArticulosCategoria = require("./DAO/articulosCategoriaDAO.js");
 var restAlmacenes = require("./DAO/almacenesDAO.js");
-var restUsuariosTipo = require("./DAO/usuariosTipoDAO.js"); 
+var restUsuariosTipo = require("./DAO/usuariosTipoDAO.js");
 var restUsuariosAcceso = require("./DAO/usuariosAccesoDAO.js");
 var restAsocTipoAcceso = require("./DAO/asocTipoAccesoDAO.js");
 var restTipoPersona = require("./DAO/tipoPersonaDAO.js");
@@ -27,43 +28,28 @@ var restIngreso = require("./DAO/ingresoDAO.js");
 var restIngresoDetalle = require("./DAO/ingresoDetalleDAO.js");
 // We execute the express
 var app = express();
+app.use(favicon(path.join(__dirname, '/dev/media/favicon.ico')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+var router = express.Router();
+app.use('/api', router);
+// express.static gaves access to a directory from the browser client
+// the __dirname directory becames "public"
+// __dirname is the current directory
+app.use(express.static(__dirname));
 
-function REST() {
-    var self = this;
-    self.connectMysql();
-}
-
-// Using prototypes <=> POO 
-REST.prototype.connectMysql = function() {
-    var self = this;
-    var pool = mysql.createPool({
-        connectionLimit: 100,
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: 'root',
-        database: 'testdb',
-        debug: false
-    });
-    pool.getConnection(function(err, connection) {
-        if (err) {
-            self.stop(err);
-        } else {
-            self.configureExpress(connection);
-        }
+const startServer = function () {
+    app.listen(8071, function () {
+        console.log(colors.green("All right ! I am alive at Port 8071."));
     });
 };
 
-REST.prototype.configureExpress = function(connection) {
-    var self = this;
-    app.use(favicon(__dirname + '/dev/media/favicon.ico'));
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(bodyParser.json());
-    var router = express.Router();
-    app.use('/api', router);
-    // express.static gaves access to a directory from the browser client
-    // the __dirname directory becames "public"
-    app.use(express.static(__dirname));
+const stop = function (err) {
+    console.log(colors.red("ISSUE WITH MYSQL \n" + err));
+    process.exit(1);
+};
+
+const configureExpress = function (router, connection, md5) {
     // Adding all the routes to our server
     new restUsuarios(router, connection, md5);
     new restArticulos(router, connection);
@@ -78,26 +64,32 @@ REST.prototype.configureExpress = function(connection) {
     new restEstado(router, connection);
     new restIngreso(router, connection);
     new restIngresoDetalle(router, connection);
-    self.startServer();
+    startServer();
 };
 
-REST.prototype.startServer = function() {
-    app.listen(8071, function() {
-        console.log(colors.green("All right ! I am alive at Port 8071."));
+const connectMysql = function (mysql, router, md5) {
+    var pool = mysql.createPool({
+        connectionLimit: 100,
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'testdb',
+        debug: false
+    });
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            stop(err);
+        } else {
+            configureExpress(router, connection, md5);
+        }
     });
 };
 
-REST.prototype.stop = function(err) {
-    console.log(colors.red("ISSUE WITH MYSQL \n" + err));
-    process.exit(1);
-};
-
-new REST();
-
+connectMysql(mysql, router, md5);
 
 // var PDFDocument = require("pdfkit");
 // var blobStream  = require("blob-stream");
 // // create a document and pipe to a blob
 // var doc = new PDFDocument();
 // var stream = doc.pipe(blobStream());
-
