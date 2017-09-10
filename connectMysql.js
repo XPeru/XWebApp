6d73d4c716a8870cf35a1bd446d70aca3e9d0331
Oscar Garay <ojcgv21@gmail.com>
2017-09-10 02:17:32 +0200

    adding listeners for debugging

--- connectMysql.js
+++ connectMysql.js
@@ -1,10 +1,9 @@
 // This module allows our server to connect and dialog with a mysql database
 const mysql = require("mysql");
-global.mysql = mysql;
 
 const createPoolMysql = function () {
     var pool = mysql.createPool({
-        connectionLimit: process.env.MYSQL_CONNECTIONLIMIT,
+        connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
         host: process.env.MYSQL_HOST,
         port: process.env.MYSQL_PORT,
         user: process.env.MYSQL_USER,
@@ -12,7 +11,26 @@ const createPoolMysql = function () {
         database: process.env.MYSQL_DATABASE_NAME,
         debug: false
     });
+
+    // the pools listeners will be trigged only if debug is true
+    pool.on('acquire', function (connection) {
+        console.info('Connection %d acquired', connection.threadId);
+    });
+
+    pool.on('connection', function (connection) {
+        connection.query('SET SESSION auto_increment_increment=1');
+    });
+
+    pool.on('enqueue', function () {
+        console.log('Waiting for available connection slot');
+    });
+
+    pool.on('release', function (connection) {
+        console.log('Connection %d released', connection.threadId);
+    });
+
     global.mySqlPool = pool;
+    global.mysql = mysql;
 };
 
 exports.createPoolMysql  = createPoolMysql;
