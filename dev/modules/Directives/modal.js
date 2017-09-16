0e1e58ae8124482df9ebdc7d8b73871755ad0072
eduapb <eduardopaucar1@gmail.com>
2017-09-16 22:14:35 +0900

    Indentation  fix

--- dev/modules/Directives/modal.js
+++ dev/modules/Directives/modal.js
@@ -1,51 +1,51 @@
 var module = angular.module('ModalDirective', []);
 
 module.controller('modalDirectiveController', ['$scope', '$rootScope', '$uibModal', function ($scope, $rootScope, $uibModal) {
-        var ctrl = this;
-        ctrl.modalData = $scope.modalData;
-        ctrl.modalSelectedData = $scope.modalSelectedData;
-        ctrl.modalExtraData = $scope.modalExtraData;
+	var ctrl = this;
+	ctrl.modalData = $scope.modalData;
+	ctrl.modalSelectedData = $scope.modalSelectedData;
+	ctrl.modalExtraData = $scope.modalExtraData;
 
-        ctrl.openModal = function () {
-            var modalInstance = $uibModal.open({
-                templateUrl: function() {
-                    return ctrl.modalData.url[ctrl.modalData.mode];
-                },
-                controller: ctrl.modalData.controller,
-                controllerAs: ctrl.modalData.controllerAs,
-                resolve: {
-                    selectedData: function () {
-                         return  ctrl.modalSelectedData;
-                    },
-                    extraData: function () {
-                        return $scope.modalExtraData;
-                    }
-                }
-            });
+	ctrl.openModal = function () {
+		var modalInstance = $uibModal.open({
+			templateUrl: function() {
+				return ctrl.modalData.url[ctrl.modalData.mode];
+			},
+			controller: ctrl.modalData.controller,
+			controllerAs: ctrl.modalData.controllerAs,
+			resolve: {
+				selectedData: function () {
+					return ctrl.modalSelectedData;
+				},
+				extraData: function () {
+					return $scope.modalExtraData;
+				}
+			}
+		});
 
-            modalInstance.result.then(function() {
-                ctrl.modalData.ctrlParent.callGetAll();
-                ctrl.modalData.ctrlParent.table.reload();
+		modalInstance.result.then(function() {
+			ctrl.modalData.ctrlParent.callGetAll();
+			ctrl.modalData.ctrlParent.table.reload();
 
-            }, function() {
-                ctrl.modalData.ctrlParent.callGetAll();
-                ctrl.modalData.ctrlParent.table.reload();
-            });
-        };
+		}, function() {
+			ctrl.modalData.ctrlParent.callGetAll();
+			ctrl.modalData.ctrlParent.table.reload();
+		});
+	};
 
-    }
+}
 ]);
 
 module.directive('modalDirective',function() {
 	return {
 		restrict: 'A',
 		controller: 'modalDirectiveController',
-        controllerAs: 'modalDirectiveCtrl',
+		controllerAs: 'modalDirectiveCtrl',
 		templateUrl: 'dev/modules/Directives/modal.html',
 		scope: {
-            modalData: '=',
-            modalSelectedData: '=',
-            modalExtraData: '='
-        }
+			modalData: '=',
+			modalSelectedData: '=',
+			modalExtraData: '='
+		}
 	};
 });
