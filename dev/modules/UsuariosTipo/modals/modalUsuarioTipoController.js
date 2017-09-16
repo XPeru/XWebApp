6f01faeccb7abc8a84625efadf2399e01fcbc78b
Oscar Garay <ojcgv21@gmail.com>
2017-09-16 15:22:40 +0200

    refacto a medias

--- dev/modules/UsuariosTipo/modals/modalUsuarioTipoController.js
+++ dev/modules/UsuariosTipo/modals/modalUsuarioTipoController.js
@@ -1,10 +1,10 @@
 "use strict";
 angular.module('UsuariosTipo')
 	.controller('modalUsuarioTipoController',
-		function($scope, $http, $timeout, $uibModalInstance, selected_tipo_usuario, UsuariosTipoServiceFactory) {
+		function($scope, $http, $timeout, $uibModalInstance, selectedData, UsuariosTipoServiceFactory) {
 
 			var ctrl = this;
-			ctrl.selected_tipo_usuario = selected_tipo_usuario;
+			ctrl.selected_tipo_usuario = selectedData;
 
 			ctrl.deleteTipoUsuario = function(tipo_usuario) {
 				UsuariosTipoServiceFactory.deleteTipoUsuario(tipo_usuario.ID_TIPO_USUARIO).then(function() {
--- dev/modules/UsuariosTipo/usuariosTipoController.js
+++ dev/modules/UsuariosTipo/usuariosTipoController.js
@@ -2,101 +2,37 @@
 
 angular.module('UsuariosTipo', ['ui.bootstrap', 'ui.grid','ui.grid.exporter', 'ui.grid.selection'])
 	.controller('usuariosTipoController', ['$scope',
-													'$window',
-													'$location',
-													'$http',
-													'$uibModal',
-													'$timeout',
-													'UsuariosTipoServiceFactory',
-													'NgTableParams',
-													'i18nService',
-													'UsuariosAccesoServiceFactory',
-	function ($scope, $window, $location, $http, $uibModal, $timeout, UsuariosTipoServiceFactory, NgTableParams, i18nService, UsuariosAccesoServiceFactory) {
+											'$window',
+											'$location',
+											'$http',
+											'$uibModal',
+											'$timeout',
+											'UsuariosTipoServiceFactory',
+											'NgTableParams',
+											'i18nService',
+											'UsuariosAccesoServiceFactory',
+											'CommonServiceFactory',
+	function ($scope, $window, $location, $http, $uibModal, $timeout, UsuariosTipoServiceFactory, NgTableParams, i18nService, UsuariosAccesoServiceFactory, CommonServiceFactory) {
 		var ctrl = this;
+		i18nService.setCurrentLang('es');
 		ctrl.tableMode = true;
 		ctrl.showAccesos = false;
-		ctrl.switchTableMode = function() {
-			ctrl.tableMode = !ctrl.tableMode;
-		};
+		CommonServiceFactory.switchTableMode(ctrl);
 
-
-		i18nService.setCurrentLang('es');
-		$scope.columns = [{ field: 'TIPO', headerCellClass: 'blue'}];
-		$scope.columns[0].displayName = 'Tipo';
-		$scope.gridOptions = {
-			exporterMenuCsv: false,
-			enableGridMenu: true,
-			enableSorting: true,
-			enableFiltering: true,
-			columnDefs: $scope.columns,
-			onRegisterApi: function(gridApi) {
-				$scope.gridApi = gridApi;
-			}
-		};
-
-		ctrl.usuariosTipoData = [{}];
-		ctrl.callGetAllTipoUsuario = function() {
+		ctrl.callGetAll = function () {
 			UsuariosTipoServiceFactory.getAllTipoUsuario().then(function(response) {
 				ctrl.usuariosTipoData = response.data.TiposUsuario;
 				$scope.gridOptions.data = response.data.TiposUsuario;
-				ctrl.usuariosTipoTable = new NgTableParams({
+				ctrl.table = new NgTableParams({
 					page: 1,
 					count: 10
 				}, {
 					total: 0,
 					data : ctrl.usuariosTipoData
-					
 				});
 			});
 		};
-		ctrl.callGetAllTipoUsuario();
 
-		ctrl.idSelectedTipoUsuario = null;
-		ctrl.setSelected = function(selectedTipoUsuario) {
-			ctrl.idSelectedTipoUsuario = selectedTipoUsuario.ID_TIPO_USUARIO;
-			ctrl.selectedTipoUsuario = selectedTipoUsuario;
-			ctrl.showAccesos = true;
-			ctrl.callGetAssosTipoAccesosByIdTipoUsuario();
-		};
-
-		ctrl.openModalTipoUsuario = function(selected_modal, selected_tipo_usuario) {
-			var modalInstance = $uibModal.open({
-				templateUrl: function() {
-					var template;
-					switch(selected_modal) {
-						case "create":
-							template = 'dev/modules/UsuariosTipo/modals/createUsuarioTipo.html';
-							break;
-						case "edit":
-							template = 'dev/modules/UsuariosTipo/modals/editUsuarioTipo.html';
-							break;
-						case "delete":
-							template = 'dev/modules/UsuariosTipo/modals/deleteUsuarioTipo.html';
-							break;
-					}
-					return template;
-				},
-				controller: 'modalUsuarioTipoController',
-				controllerAs: 'modalUsuarioTipoCtrl',
-				resolve : {
-					selected_tipo_usuario : function() {
-						return selected_tipo_usuario;
-					}
-				}
-			});
-
-			modalInstance.result.then(function() {
-				ctrl.callGetAllTipoUsuario();
-				ctrl.usuariosTipoTable.reload();
-				
-			}, function() {
-				ctrl.callGetAllTipoUsuario();
-				ctrl.usuariosTipoTable.reload();
-			});
-		};
-
-
-		ctrl.assoTipoAccesoData = [{}];
 		ctrl.callGetAssosTipoAccesosByIdTipoUsuario = function() {
 			UsuariosTipoServiceFactory.getAssosTipoAccesosByIdTipoUsuario(ctrl.idSelectedTipoUsuario).then(function(response) {
 				ctrl.assoTipoAccesoData = response.data.Assos;
@@ -109,6 +45,49 @@ angular.module('UsuariosTipo', ['ui.bootstrap', 'ui.grid','ui.grid.exporter', 'u
 				});
 			});
 		};
+
+		$scope.columns = [
+			CommonServiceFactory.formatColumn('Tipo','TIPO', 'blue', 'text'),
+			CommonServiceFactory.buttons()
+		];
+
+		$scope.gridOptions = {
+			exporterMenuCsv: false,
+			enableGridMenu: true,
+			enableSorting: true,
+			enableFiltering: true,
+			columnDefs: $scope.columns,
+			onRegisterApi: function(gridApi) {
+				$scope.gridApi = gridApi;
+			}
+		};
+
+		ctrl.usuariosTipoData = [];
+
+		ctrl.callGetAll();
+
+		ctrl.idSelectedTipoUsuario = null;
+		ctrl.setSelected = function(selectedTipoUsuario) {
+			ctrl.idSelectedTipoUsuario = selectedTipoUsuario.ID_TIPO_USUARIO;
+			ctrl.selectedTipoUsuario = selectedTipoUsuario;
+			ctrl.showAccesos = true;
+			ctrl.callGetAssosTipoAccesosByIdTipoUsuario();
+		};
+
+		ctrl.modal = {
+			url: {
+				create : 'dev/modules/UsuariosTipo/modals/createUsuarioTipo.html',
+				edit: 'dev/modules/UsuariosTipo/modals/editUsuarioTipo.html',
+				delete: 'dev/modules/UsuariosTipo/modals/deleteUsuarioTipo.html'
+			},
+			controller: 'modalUsuarioTipoController',
+			controllerAs: 'modalUsuarioTipoCtrl',
+			id: 'ID_TIPO_USUARIO',
+			ctrlParent: ctrl
+		};
+		CommonServiceFactory.modal(ctrl);
+		ctrl.assoTipoAccesoData = [{}];
+
 
 		ctrl.usuariosAccesoData = [{}];
 		ctrl.callGetAllAccesoUsuario = function() {
@@ -123,7 +102,7 @@ angular.module('UsuariosTipo', ['ui.bootstrap', 'ui.grid','ui.grid.exporter', 'u
 			var modalInstance = $uibModal.open({
 				templateUrl: function() {
 					return 'dev/modules/UsuariosTipo/modals/editAssoTipoAcceso.html';
-					
+
 				},
 				controller: 'modalAssoTipoAccesoController',
 				controllerAs: 'modalAssoTipoAccesoCtrl',
@@ -143,7 +122,7 @@ angular.module('UsuariosTipo', ['ui.bootstrap', 'ui.grid','ui.grid.exporter', 'u
 			modalInstance.result.then(function() {
 				ctrl.callGetAssosTipoAccesosByIdTipoUsuario();
 				ctrl.assoTipoAccesoTable.reload();
-				
+
 			}, function() {
 				ctrl.callGetAssosTipoAccesosByIdTipoUsuario();
 				ctrl.assoTipoAccesoTable.reload();
