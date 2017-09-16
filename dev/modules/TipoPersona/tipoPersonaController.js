516a9c9862ff742af711e03dd9654844fad23e54
Oscar Garay <ojcgv21@gmail.com>
2017-09-16 17:22:05 +0200

    refacto modulo tipo persona

--- dev/modules/TipoPersona/tipoPersonaController.js
+++ dev/modules/TipoPersona/tipoPersonaController.js
@@ -2,87 +2,66 @@
 
 angular.module('TipoPersona', ['ui.bootstrap', 'ui.grid','ui.grid.exporter', 'ui.grid.selection'])
     .controller('tipoPersonaController', ['$scope',
-                                                    '$location',
-                                                    '$http',
-                                                    '$uibModal',
-                                                    '$timeout',
-                                                    'TipoPersonaServiceFactory',
-                                                    'NgTableParams',
-                                                    'i18nService',
-        function($scope, $location, $http, $uibModal, $timeout, TipoPersonaServiceFactory, NgTableParams, i18nService) {
-            var ctrl = this;
-            ctrl.tableMode = true;
-            ctrl.switchTableMode = function() {
-                ctrl.tableMode = !ctrl.tableMode;
-            };
+										'$location',
+										'$http',
+										'$uibModal',
+										'$timeout',
+										'TipoPersonaServiceFactory',
+										'NgTableParams',
+										'i18nService',
+										'CommonServiceFactory',
+		function($scope, $location, $http, $uibModal, $timeout, TipoPersonaServiceFactory, NgTableParams, i18nService, CommonServiceFactory) {
+			var ctrl = this;
+			i18nService.setCurrentLang('es');
 
-            i18nService.setCurrentLang('es');
-            $scope.columns = [{ field: 'DESCRIPCION', headerCellClass: 'blue'}];
-            $scope.columns[0].displayName = 'Descripcion';
-            $scope.gridOptions = {
-                exporterMenuCsv: false,
-                enableGridMenu: true,
-                enableSorting: true,
-                enableFiltering: true,
-                columnDefs: $scope.columns,
-                onRegisterApi: function(gridApi) {
-                    $scope.gridApi = gridApi;
-                }
-            };
+			ctrl.callGetAll = function() {
+				TipoPersonaServiceFactory.getAllTipoPersona().then(function(response) {
+					ctrl.tipoPersonaData = response.data.TipoPersona;
+					$scope.gridOptions.data = response.data.TipoPersona;
+					ctrl.table = new NgTableParams({
+						page: 1,
+						count: 10
+					}, {
+						data: ctrl.tipoPersonaData
+					});
+				});
+			};
 
-            ctrl.tipoPersonaData = [{}];
-            ctrl.callGetAllTipoPersona = function() {
-                TipoPersonaServiceFactory.getAllTipoPersona().then(function(response) {
-                    ctrl.tipoPersonaData = response.data.TipoPersona;
-                    $scope.gridOptions.data = response.data.TipoPersona;
-                    ctrl.tipoPersonaTable = new NgTableParams({
-                        page: 1,
-                        count: 10
-                    }, {
-                        data: ctrl.tipoPersonaData
-                    });
-                });
-            };
-            ctrl.callGetAllTipoPersona();
+			ctrl.modal = {
+				url: {
+					create : 'dev/modules/TipoPersona/modals/createTipoPersona.html',
+					edit: 'dev/modules/TipoPersona/modals/editTipoPersona.html',
+					delete: 'dev/modules/TipoPersona/modals/deleteTipoPersona.html'
+				},
+				controller: 'modalTipoPersonaController',
+				controllerAs: 'modalTipoPersonaCtrl',
+				id: 'ID_TIPO_PERSONA',
+				ctrlParent: ctrl
+			};
 
-            ctrl.idSelectedTipoPersona = null;
-            ctrl.setSelected = function(idSelectedTipoPersona) {
-                ctrl.idSelectedTipoPersona = idSelectedTipoPersona;
-            };
+			CommonServiceFactory.modal(ctrl);
+			CommonServiceFactory.switchTableMode(ctrl);
+			CommonServiceFactory.setSelected(ctrl, "idSelectedTipoPersona");
 
-            ctrl.openModalTipoPersona = function(selected_modal, selectedTipoPersona) {
-                var modalInstance = $uibModal.open({
-                    templateUrl: function() {
-                        var template;
-                        switch(selected_modal) {
-                            case "create":
-                                template = 'dev/modules/TipoPersona/modals/createTipoPersona.html';
-                                break;
-                            case "edit":
-                                template = 'dev/modules/TipoPersona/modals/editTipoPersona.html';
-                                break;
-                            case "delete":
-                                template = 'dev/modules/TipoPersona/modals/deleteTipoPersona.html';
-                                break;
-                        }
-                        return template;
-                    },
-                    controller: 'modalTipoPersonaController',
-                    controllerAs: 'modalTipoPersonaCtrl',
-                    resolve : {
-                        selectedTipoPersona : function() {
-                            return selectedTipoPersona;
-                        }
-                    }
-                });
+			ctrl.tableMode = true;
+			ctrl.tipoPersonaData = [];
+			ctrl.idSelectedTipoPersona = null;
+			ctrl.callGetAll();
 
-                modalInstance.result.then(function() {
-                    ctrl.callGetAllTipoPersona();
-                    ctrl.tipoPersonaTable.reload();
-                }, function() {
-                    ctrl.callGetAllTipoPersona();
-                    ctrl.tipoPersonaTable.reload();
-                });
-            };
-        }
+			$scope.columns = [
+				CommonServiceFactory.formatColumn('Descripcion','DESCRIPCION', 'blue', 'text'),
+				CommonServiceFactory.buttons()
+			];
+
+			$scope.gridOptions = {
+				exporterMenuCsv: false,
+				enableGridMenu: true,
+				enableSorting: true,
+				enableFiltering: true,
+				columnDefs: $scope.columns,
+				onRegisterApi: function(gridApi) {
+					$scope.gridApi = gridApi;
+				}
+			};
+		}
 ]);
