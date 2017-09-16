0165610c3f880b6f7ec79e1d48d3d5ee750663f6
eduapb <eduardopaucar1@gmail.com>
2017-09-17 00:40:37 +0900

    Refactoring Cliente 2

--- dev/modules/Cliente/clienteController.js
+++ dev/modules/Cliente/clienteController.js
@@ -31,9 +31,6 @@ angular.module('Cliente', ['ui.bootstrap', 'ui.grid','ui.grid.exporter', 'ui.gri
 			ctrl.callGetAll();
 			ctrl.tableMode = true;
 			ctrl.idSelectedCliente = null;
-			// ctrl.setSelected = function(idSelectedCliente) {
-			// 	ctrl.idSelectedCliente = idSelectedCliente;
-			// };
 
 			ctrl.callGetTipoPersonaByDesc = function() {
 				TipoPersonaServiceFactory.getTipoPersonaByDesc('Cliente').then(function(response) {
@@ -58,50 +55,6 @@ angular.module('Cliente', ['ui.bootstrap', 'ui.grid','ui.grid.exporter', 'ui.gri
 			CommonServiceFactory.switchTableMode(ctrl);
 			CommonServiceFactory.setSelected(ctrl, "idSelectedCliente");
 
-			// ctrl.openModalCliente = function(selected_modal, selectedCliente) {
-			// 	var modalInstance = $uibModal.open({
-			// 		templateUrl: function() {
-			// 			var template;
-			// 			switch(selected_modal) {
-			// 				case "create":
-			// 					template = 'dev/modules/Cliente/modals/createCliente.html';
-			// 					break;
-			// 				case "edit":
-			// 					template = 'dev/modules/Cliente/modals/editCliente.html';
-			// 					break;
-			// 				case "delete":
-			// 					template = 'dev/modules/Cliente/modals/deleteCliente.html';
-			// 					break;
-			// 			}
-			// 			return template;
-			// 		},
-			// 		controller: 'modalClienteController',
-			// 		controllerAs: 'modalClienteCtrl',
-			// 		resolve : {
-			// 			selectedCliente : function() {
-			// 				return selectedCliente;
-			// 			},
-			//
-			// 			tipoPersona: function() {
-			// 				return ctrl.tipoPersona;
-			// 			}
-			// 		}
-			// 	});
-			//
-			// 	modalInstance.result.then(function() {
-			// 		ctrl.callGetAllCliente();
-			// 		ctrl.clienteTable.reload();
-			// 	}, function() {
-			// 		ctrl.callGetAllCliente();
-			// 		ctrl.clienteTable.reload();
-			// 	});
-			// };
-
-
-			// ctrl.switchTableMode = function() {
-			// 	ctrl.tableMode = !ctrl.tableMode;
-			// };
-
 			$scope.columns = [
 				CommonServiceFactory.formatColumn('Nombre','NOMBRE','blue','text'),
 				CommonServiceFactory.formatColumn('Email','EMAIL','blue','text'),
@@ -114,25 +67,6 @@ angular.module('Cliente', ['ui.bootstrap', 'ui.grid','ui.grid.exporter', 'ui.gri
 				CommonServiceFactory.formatColumn('Telefono','TELEFONO','blue','text'),
 				CommonServiceFactory.buttons()
 			];
-
-			// $scope.columns = [{ field: 'NOMBRE', headerCellClass: 'blue'},
-			// 					{ field: 'EMAIL', headerCellClass: 'blue'},
-			// 					{ field: 'RUC', headerCellClass: 'blue'},
-			// 					{ field: 'NUMERO_CUENTA', headerCellClass: 'blue'},
-			// 					{ field: 'DIRECCION_CALLE', headerCellClass: 'blue'},
-			// 					{ field: 'DIRECCION_DISTRITO', headerCellClass: 'blue'},
-			// 					{ field: 'DIRECCION_DEPARTAMENTO', headerCellClass: 'blue'},
-			// 					{ field: 'DIRECCION_COMPLEMENTO', headerCellClass: 'blue'},
-			// 					{ field: 'TELEFONO', headerCellClass: 'blue'}];
-			// $scope.columns[0].displayName = 'Nombre';
-			// $scope.columns[1].displayName = 'Email';
-			// $scope.columns[2].displayName = 'RUC';
-			// $scope.columns[3].displayName = 'Numero de cuenta';
-			// $scope.columns[4].displayName = 'Calle';
-			// $scope.columns[5].displayName = 'Distrito';
-			// $scope.columns[6].displayName = 'Departamento';
-			// $scope.columns[7].displayName = 'Complemento';
-			// $scope.columns[8].displayName = 'Telefono';
 
 			$scope.gridOptions = {
 				exporterMenuCsv: false,
