39f5018dac44afe22ca76d473d2f2cc72c1b4429
eduapb <eduardopaucar1@gmail.com>
2017-09-17 13:25:11 +0900

    ConnectMysql - Indentation corrected

--- connectMysql.js
+++ connectMysql.js
@@ -2,43 +2,43 @@
 const mysql = require("mysql");
 
 const createPoolMysql = function () {
-    var pool = mysql.createPool({
-        connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
-        host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE_NAME,
-        debug: false
-    });
+	var pool = mysql.createPool({
+		connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
+		host: process.env.MYSQL_HOST,
+		port: process.env.MYSQL_PORT,
+		user: process.env.MYSQL_USER,
+		password: process.env.MYSQL_PASSWORD,
+		database: process.env.MYSQL_DATABASE_NAME,
+		debug: false
+	});
 
-    // the pools listeners will be trigged only if debug is true
-    pool.on('acquire', function (connection) {
-        console.info('Connection %d acquired', connection.threadId);
-    });
+	// the pools listeners will be trigged only if debug is true
+	pool.on('acquire', function (connection) {
+		console.info('Connection %d acquired', connection.threadId);
+	});
 
-    pool.on('connection', function (connection) {
-        connection.query('SET SESSION auto_increment_increment=1');
-    });
+	pool.on('connection', function (connection) {
+		connection.query('SET SESSION auto_increment_increment=1');
+	});
 
-    pool.on('enqueue', function () {
-        console.log('Waiting for available connection slot');
-    });
+	pool.on('enqueue', function () {
+		console.log('Waiting for available connection slot');
+	});
 
-    pool.on('release', function (connection) {
-        console.log('Connection %d released', connection.threadId);
-    });
+	pool.on('release', function (connection) {
+		console.log('Connection %d released', connection.threadId);
+	});
 
-		pool.query("SELECT 'Testing connection to DB ... OK!' AS test_conn", function(err, rows) {
-		  if(err) {
-				console.log('Testing connection to DB ... Failed!');
-				throw err;
-			}
-		  else console.log(rows[0].test_conn);
-		});
+	pool.query("SELECT 'Testing connection to DB ... OK!' AS test_conn", function(err, rows) {
+		if(err) {
+			console.log('Testing connection to DB ... Failed!');
+			throw err;
+		}
+		else console.log(rows[0].test_conn);
+	});
 
-    global.mySqlPool = pool;
-    global.mysql = mysql;
+	global.mySqlPool = pool;
+	global.mysql = mysql;
 };
 
 exports.createPoolMysql  = createPoolMysql;
