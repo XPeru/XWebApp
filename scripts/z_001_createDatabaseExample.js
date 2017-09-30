24de9b61276da80b53bee1e14c50fa971f0f78fd
eduapb <eduardopaucar1@gmail.com>
2017-09-30 17:05:05 +0900

    ToDo: Exceptions handling #4

--- scripts/z_001_createDatabaseExample.js
+++ scripts/z_001_createDatabaseExample.js
@@ -1,30 +1,22 @@
 var connection = require("../utils/standalone.js").connection();
 
 connection.beginTransaction(function (err) {
-
-    if (err) {
-        throw err;
-    }
-
-    var db_name = 'new_schema';
-    connection.query('CREATE DATABASE IF NOT EXISTS ' + db_name, function (error, results, fields) {
-        if (error) {
-            return connection.rollback(function() {
-                throw error;
-            });
-        }
-
-        connection.commit(function (err) {
-            if (err) {
-                return connection.rollback(function () {
-                    throw err;
-                });
-            }
-            console.log('Database ' + db_name + ' created');
-
-            connection.end(function (err) {
-                console.log('Connection ended');
-            });
-        });
-    });
+	if (err) {
+		throw err;
+	}
+	var db_name = 'new_schema';
+	new Promise (function(resolve, reject) {
+		connection.query('CREATE DATABASE IF NOT EXISTS ' + db_name);
+	})
+	.then(new Promise(function() {
+		connection.commit();
+	}))
+	.then(new Promise(function() {
+		console.log('Database ' + db_name + ' created');
+		connection.end();
+		console.log('Connection closed normally');
+	}));
+	// catch(function(e){
+	// 	connection.rollback();
+	// });
 });
